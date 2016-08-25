87388e18d69b4833cb31220bdb30b02cc02a77dc
Keegan Kelly <keegankelly1@gmail.com>
2016-08-25 07:45:08 -0500

    Added button and combat log to html; Character select working;

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,24 @@
 $(document).ready(function(){
-	var finn = $("#finn");
-	var jake = $("#jake");
-	var gunter = $("#gunter");
-	var iceking = $("#iceking");
-	var characters = [finn, jake, gunter, iceking];
+	var $finn = $("#finn");
+	var $jake = $("#jake");
+	var $gunter = $("#gunter");
+	var $iceking = $("#iceking");
+	var $button = $(".button");
+	var characters = [$finn, $jake, $gunter, $iceking];
 	var playerCheck = false;
 	var enemyCheck = false;
+
+	var finn{
+		hp: 20,
+		attack: 5,
+		counterAttack: 2,
+	};
+
+	var jake{
+		hp: 20,
+		attack: 1,
+		counterAttack: 1;
+	};
 	
 	function setPlayer(character){
 		if (playerCheck === false){
@@ -13,45 +26,60 @@ $(document).ready(function(){
 			playerCheck = true;
 
 			$(character).appendTo("#player");
+
 			
 			for(var i = 0; i < characters.length; i++){
 				
 				if(characters[i] !== character){
 					
 					$(characters[i]).appendTo(".enemies");
-						console.log(characters[i]);
 				};
 			};	
 		}
+
 		else if(playerCheck === true && enemyCheck === false){
 
 			enemyCheck = true;
 
 			$(character).appendTo("#enemy");
 		}
+
 		else{
 			alert("You must defeat your chosen enemy!");
 		}
 
 	};
 
+	function combat(player, enemy){
+		enemy.hp - player.attack;
+		console.log(enemy.hp);
+		console.log(player.hp);
+	};
+
 	
 
-	$(finn).click(function(){
-		setPlayer(finn);
+	$($finn).click(function(){
+		setPlayer($finn);
+		console.log(player);
 	});
 
-	$(jake).click(function(){
-		setPlayer(jake);
+	$($jake).click(function(){
+		setPlayer($jake);
+		console.log(enemy);
 	}); 
 
-	$(gunter).click(function(){
-		setPlayer(gunter);
+	$($gunter).click(function(){
+		setPlayer($gunter);
 	});
 
-	$(iceking).click(function(){
-		setPlayer(iceking);
+	$($iceking).click(function(){
+		setPlayer($iceking);
 	});	
+
+	$($button).click(function(){
+		console.log("Attack!");
+		combat(finn, jake);
+	});
 
 });
 
